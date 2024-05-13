515479659ec6525bb0dc6cead347f7552f249939
jonahbebko <jonahbebko@outlook.com>
2024-05-13 03:59:00 +0000

    style: border and color stuff, logo anim

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,19 @@ function App() {
         </div>
         <div className="App-header-library">
           <p>App-header-library</p>
+          <p>(no peeking!)</p>
         </div>
       </div>
       <div className="App-body">
-        <h1>streamit!</h1>
+        <div className="App-body-logos">
+          <img src="logot512.png" alt="logo" />
+          <h1>streamit!</h1>
+        </div>
         <p>this project is undergoing a complete rebuild from the ground up,<br />
-        mostly due to the abandomnemt of <code>create-react-app</code> which made<br />
+        mostly due to the abandonment of <code>create-react-app</code> which made<br />
         lots of problems with PWA caching and using new packages. however, I kept<br />
-        most of the important scripting stuff, so streamit! should be back in a few<br />
-        days. check back for updates!</p>
+        most of the important scripting stuff, so the streamit! you know and love<br />
+        should be back in a few days. check back for updates! <code>\(^o^)/</code></p>
       </div>
     </div>
   )
