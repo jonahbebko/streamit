458ffdaa853a6d1d837e7fbba1a4baa53ac8bed6
jonahbebko <jonahbebko@outlook.com>
2024-05-14 18:29:37 +0000

    fix: improper PWA touch icon scaling

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,38 @@ export default defineConfig({
       short_name: 'streamit!',
       homepage: '/streamit/',
       description: 'web-based music streaming service',
+      background_color: '#000000',
       theme_color: '#000000',
+      icons: [
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'manifest-icon-512.maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: 'manifest-icon-192.maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
     },
 
     workbox: {
