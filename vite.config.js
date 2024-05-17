c22677ce21c81e78410b6739b67fc9016c2b4c9f
jonahbebko <jonahbebko@outlook.com>
2024-05-17 01:53:22 +0000

    fix: incorrect server origin

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    origin: 'https://127.0.0.1/streamit',
+    host: 'true',
+  },
+  
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
