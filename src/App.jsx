d4c970c3b6d7903f9e605235370973e63de4da6b
jonahbebko <jonahbebko@outlook.com>
2024-05-18 02:45:30 +0000

    feat: uid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState } from 'react'
 import PWAPrompt from 'react-ios-pwa-prompt'
+import sha256 from 'js-sha256'
 import Welcome from './pages/Welcome'
 import Home from './pages/Home'
 import Search from './pages/Search'
@@ -8,10 +9,11 @@ import Settings from './pages/Settings'
 
 function App() {
 
-  if (window.matchMedia('(display-mode: standalone)').matches) {
-    console.log('PWA is running in standalone mode');
+  if (localStorage.getItem('uid') === null) {
+    localStorage.setItem('uid', sha256.create().update(Date.now().toString()).hex());
+    console.log(`first visit, creating uid ${localStorage.getItem('uid')}`);
   } else {
-    console.log('PWA is running in browser window');
+    console.log(`logged in as user ${localStorage.getItem('uid')}`);
   }
 
   const [welcome, setWelcome] = useState(true);
@@ -47,33 +49,33 @@ function App() {
   return (
     <>
       <PWAPrompt
-        copyBody="streamit! has app functionality. Add it to your home screen to use in the background."
-        copyShareButtonLabel="1. Press the Share button."
-        copyAddHomeButtonLabel="2. Press Add to Home Screen."
-        copyClosePrompt="Close" />
-      <div className="App-root">
-        <div className="App-header">
-          <div className="App-header-top" onClick={rsetWelcome}>
-            <img src="logot512.png" alt="logo" />
+        copyBody='streamit! has app functionality. Add it to your home screen to use in the background.'
+        copyShareButtonLabel='1. Press the Share button.'
+        copyAddHomeButtonLabel='2. Press Add to Home Screen.'
+        copyClosePrompt='Close' />
+      <div className='App-root'>
+        <div className='App-header'>
+          <div className='App-header-top' onClick={rsetWelcome}>
+            <img src='logot512.png' alt='logo' />
             <h1>streamit!</h1>
             <p>pre-alpha 0.1.0</p>
           </div>
-          <div className="App-header-bottom">
-            <button className="Banner-Home" style={{color: home ? "white" : "grey"}} onClick={rsetHome}>
-              <span class="material-symbols-outlined">home</span>
+          <div className='App-header-bottom'>
+            <button className='Banner-Home' style={{color: home ? 'white' : 'grey'}} onClick={rsetHome}>
+              <span class='material-symbols-outlined'>home</span>
               <p>home</p>
             </button>
-            <button className="Banner-Search" style={{color: search ? "white" : "grey"}} onClick={rsetSearch}>
-              <span class="material-symbols-outlined">search</span>
+            <button className='Banner-Search' style={{color: search ? 'white' : 'grey'}} onClick={rsetSearch}>
+              <span class='material-symbols-outlined'>search</span>
               <p>search</p>
             </button>
-            <button className="Banner-Settings" style={{color: settings ? "white" : "grey"}} onClick={rsetSettings}>
-              <span class="material-symbols-outlined">settings</span>
+            <button className='Banner-Settings' style={{color: settings ? 'white' : 'grey'}} onClick={rsetSettings}>
+              <span class='material-symbols-outlined'>settings</span>
               <p>settings</p>
             </button>
           </div>
         </div>
-        <div className="App-body">
+        <div className='App-body'>
           {welcome ? <Welcome /> : null}
           {home ? <Home /> : null}
           {search ? <Search /> : null}
