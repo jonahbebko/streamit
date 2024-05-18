17edcb47e2489eac415a9ced03a18dd39d8020e7
jonahbebko <jonahbebko@outlook.com>
2024-05-18 02:44:14 +0000

    fix: smaller chunks with nodePolyfills plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,19 @@ export default defineConfig({
     host: 'true',
   },
   
-  plugins: [react(), VitePWA({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].split('@')[0]
+          }
+        },
+      },
+    },
+  },
+
+  plugins: [react(), nodePolyfills(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
 
