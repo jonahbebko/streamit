5ab16a222bd1f0362b7328a0f6625b700c8eb42d
jonahbebko <jonahbebko@outlook.com>
2024-05-27 22:36:14 +0000

    fix: more HTML character replacements

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -8,8 +8,16 @@ class Song extends React.Component{
             <div className='song'>
                 <img src={i.snippet.thumbnails.high.url} />
                 <div>
-                    <h3>{i.snippet.title.replace('&#39;', '\'')}</h3>
-                    <p>{i.snippet.channelTitle.replace('&#39;', '\'')}</p>
+                    <h3>{i.snippet.title
+                                    .replace('&#39;', '\'')
+                                    .replace('&quot;', '"')
+                                    .replace('&amp;', '&')
+                        }</h3>
+                    <p>{i.snippet.channelTitle
+                                    .replace('&#39;', '\'')
+                                    .replace('&quot;', '"')
+                                    .replace('&amp;', '&')
+                        }</p>
                 </div>
             </div>
         )
