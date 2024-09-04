bdab38069fb8939c379663f8660070471ca1ff86
jonahbebko <jonahbebko@outlook.com>
2024-09-04 12:50:58 +0000

    chore: change welcome paragraph

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,12 +4,13 @@ function Welcome() {
     return (
         <div className='Welcome'>
             <h1>hello</h1>
-            <p>this project is undergoing a complete rebuild from the ground up,<br />
-            mostly due to the abandonment of <code>create-react-app</code> which made<br />
-            lots of problems with PWA caching and using new packages. however, I kept<br />
-            most of the important scripting stuff, so the streamit! you know and love<br />
-            should be back in a few days. check back for updates! <code>\(^o^)/</code></p>
-            <p>also... peep the pretty rainbow :o</p>
+            <p>this project is undergoing a complete rebuild from the ground up,
+            mostly due to the abandonment of <code>create-react-app</code> which made
+            lots of problems with PWA caching and using new packages.</p>
+            <p>streamit! is currently a dead project - any continual work will depend
+            on to what degree the project would be useful to other people. i will
+            continue to make small updates every now and then, but don't expect
+            any big progress for a while. stay tuned! <code>\(^o^)/</code></p>
         </div>
     );
 }
