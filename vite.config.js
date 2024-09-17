075f5db2de2ce776b6c87bffda72ac7954fb9289
jonahbebko <jonahbebko@outlook.com>
2024-09-17 08:18:43 -0400

    fix(server): disable non-local access

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     origin: 'https://127.0.0.1/streamit',
-    host: 'true',
+    //host: 'true',
   },
   
   build: {
